286efcface4d074cb7a306ec7c96536bb3ec67f5
kimmjieun <43941347+kimmjieun@users.noreply.github.com>
2021-02-23 16:44:47 +0900

    문제해결

---
+++ BOJ/단계별로 풀어보기/23단계 DFS와 BFS/1260번 DFS와BFS.py
@@ -0,0 +1,34 @@
+n,m,v=map(int,input().split())
+matrix=[[0]*(n+1) for i in range(n+1)]
+
+for i in range(m):
+    a,b = map(int,input().split())
+    matrix[a][b]=matrix[b][a]=1
+
+visit_list=[0]*(n+1)
+
+def dfs(v):
+    # queue=[v] #큐에 넣기
+    # v=queue.pop(0) #방문한거 pop
+    visit_list[v]=1 # 방문했다는 표시
+    print(v,end=' ') # 출력
+    for i in range(1,n+1):
+        if visit_list[i]==0 and matrix[v][i]==1: #방문하지않은 노드이면서 연결된 노드일때
+            dfs(i)
+#dfs 는 깊이탐색 이어달리기 느낌 한정점에서 다른정점 그리고  그 정점과 이어진 정점 연결
+
+#와이파이느낌? 한정점과 이어진 모든 정점을 들른후 다음정점도 마찬가지로 시작
+def bfs(v):
+    queue=[v] #들려야할 정점 저장
+    visit_list[v]=0 #방문한점 0으로 표시 위에서 1로바꿔놔서 여기선 0으로 표기
+    while queue:
+        v=queue.pop(0) #첫번째 데이터 pop
+        print(v,end=' ') #
+        for i in range(1,n+1):
+            if(visit_list[i]==1 and matrix[v][i]==1):
+                queue.append(i)
+                visit_list[i]=0
+
+dfs(v)
+print()
+bfs(v)
